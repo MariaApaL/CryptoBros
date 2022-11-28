285f2f1e58d94516bcb221d6763a5c54fd62f00f
MariaApaL <mluquelopezwrk@gmail.com>
2022-11-28 11:09:31 +0100

    crypto bros

---
+++ src/app/componentes/cabecera/cabecera.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecera',
+  templateUrl: './cabecera.component.html',
+  styleUrls: ['./cabecera.component.scss'],
+})
+export class CabeceraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,17 +3,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { ComponentesModule } from '../componentes/componentes.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+  
+    Tab1PageRoutingModule,
+    ComponentesModule
   ],
   declarations: [Tab1Page]
 })
