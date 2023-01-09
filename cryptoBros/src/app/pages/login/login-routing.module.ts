31009e517894acfef74073a995d27fc1e89d5995
Hugo Fernandez Garcia <hugocantito@gmail.com>
2023-01-09 11:10:29 +0100

    Retardo Solucionado

--- cryptoBros/src/app/pages/login/login-routing.module.ts
+++ cryptoBros/src/app/pages/login/login-routing.module.ts
@@ -7,11 +7,12 @@ const routes: Routes = [
   {
     path: '',
     component: LoginPage
-  },
-  {
-    path: 'signup',
-    loadChildren: () => import('../signup/signup.module').then( m => m.SignupPageModule)
   }
+  // ,
+  // {
+  //   path: 'signup',
+  //   loadChildren: () => import('../signup/signup.module').then( m => m.SignupPageModule)
+  // }
 ];
 
 @NgModule({
