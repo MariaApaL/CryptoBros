1650cf74a57b24b50d4e2c2f7a7c381eec9cda6d
Alejandro <aledelarosa2@gmail.com>
2023-02-13 20:36:34 +0100

    creacion de newUser e interfaz User

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,21 +1,23 @@
 import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+
 class userController{
 
-    getData(req: Request, res:Response){
-        console.log(req.query);
-        let user = req.query.user;
-        if(user){
-            return res.status(200).json({
-                status:'ok',
-                message:"el usuario es"+ user
-            });
-        }else{
-            return res.status(500).json({
-                status:'fail',
-                message:'no hay usuario'
-            })
-        }
-    }
+    // getData(req: Request, res:Response){
+    //     console.log(req.query);
+    //     let user = req.query.user;
+    //     if(user){
+    //         return res.status(200).json({
+    //             status:'ok',
+    //             message:"el usuario es"+ user
+    //         });
+    //     }else{
+    //         return res.status(500).json({
+    //             status:'fail',
+    //             message:'no hay usuario'
+    //         })
+    //     }
+    // }
 
     login(req: Request, res:Response){
         console.log(req.body);
@@ -34,6 +36,35 @@ class userController{
                 message:'no hay usuario'
             })
         }
+    }
+
+    newUser(req:Request, res:Response){
+        let pwdPlana = req.body.pwd;
+        const hash = bcrypt.hashSync(pwdPlana,10)
+        
+        const newUser = {
+                user: req.body.user,
+                email: req.body.email,
+                pwd: hash,
+                age: req.body.age
+            }
+
+            User.create(newUser, (err, userDB)=>{
+                if(err){
+                    return res.status(200).json({
+                        status:'fail',
+                        message:'error al crear el usuario',
+                        err
+                    })
+                }else{
+                    return res.status(200).json({
+                        status:'ok',
+                        message:'usuario creado correctamente',
+                        userDB
+                    })
+                }
+            })
+        
     }
 }
 
